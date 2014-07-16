1b2c8b94469c6f54060c152ab56c510aaa753633
Bryan Stearns <bryanstearns@gmail.com>
2014-07-15 22:55:45 -0700

    Run it every minute

--- trimet-insights.rb
+++ trimet-insights.rb
@@ -10,46 +10,49 @@ INSIGHTS_INSERT_KEY = ENV['INSIGHTS_INSERT_KEY']
 INSIGHTS_API_URL    = ENV['INSIGHTS_API_URL']
 TRIMET_STOPS        = ENV['TRIMET_STOPS']
 
-response = HTTParty.get(
-    "http://developer.trimet.org/ws/V1/arrivals/",
-    :query => {:locIDs => TRIMET_STOPS, :appID => TRIMET_APP_ID, :json => true})
+loop do
+  response = HTTParty.get(
+      "http://developer.trimet.org/ws/V1/arrivals/",
+      :query => {:locIDs => TRIMET_STOPS, :appID => TRIMET_APP_ID, :json => true})
 
-locations = response['resultSet']['location'].inject({}) do |locations, loc|
-  locations[loc['locid']] = loc['desc']
-  locations
+  locations = response['resultSet']['location'].inject({}) do |locations, loc|
+    locations[loc['locid']] = loc['desc']
+    locations
+  end
+
+  events = []
+  response['resultSet']['arrival'].each do |arrival|
+
+    next unless arrival['estimated']
+    estimated_arrival = Time.parse(arrival['estimated'])
+    scheduled_arrival = Time.parse(arrival['scheduled'])
+    # puts arrival['estimated']
+    delay = scheduled_arrival - estimated_arrival
+    # Consider faking the timestamp as time of estimate recording
+    # timestamp = Time.parse(arrival['blockPosition']['at'])
+
+    events << {
+      :eventType         => 'TriMet Status',
+      :eventVersion      => 1,
+      # :timestamp         => timestamp,
+      :status            => arrival['status'],
+      :estimated_arrival => estimated_arrival.xmlschema,
+      :scheduled_arrival => scheduled_arrival.xmlschema,
+      :delay             => delay.to_i,
+      :shortsign         => arrival['shortSign'],
+      :direction         => arrival['dir'],
+      :route             => arrival['route'],
+      :stop_id           => arrival['locid'],
+      :stop_desc   => locations[arrival['locid']],
+      :detoured          => arrival['detour'].to_s,
+    }
+
+  end
+
+  puts HTTParty.post(INSIGHTS_API_URL,
+              :body    => Yajl::Encoder.encode(events),
+              :headers => {'Content-Type' => 'application/json',
+                           'X-Insert-Key' => INSIGHTS_INSERT_KEY})
+
+  sleep(60)
 end
-
-
-events = []
-response['resultSet']['arrival'].each do |arrival|
-
-  next unless arrival['estimated']
-  estimated_arrival = Time.parse(arrival['estimated'])
-  scheduled_arrival = Time.parse(arrival['scheduled'])
-  # puts arrival['estimated']
-  delay = scheduled_arrival - estimated_arrival
-  # Consider faking the timestamp as time of estimate recording
-  # timestamp = Time.parse(arrival['blockPosition']['at'])
-
-  events << {
-    :eventType         => 'TriMet Status',
-    :eventVersion      => 1,
-    # :timestamp         => timestamp,
-    :status            => arrival['status'],
-    :estimated_arrival => estimated_arrival.xmlschema,
-    :scheduled_arrival => scheduled_arrival.xmlschema,
-    :delay             => delay.to_i,
-    :shortsign         => arrival['shortSign'],
-    :direction         => arrival['dir'],
-    :route             => arrival['route'],
-    :stop_id           => arrival['locid'],
-    :stop_desc   => locations[arrival['locid']],
-    :detoured          => arrival['detour'].to_s,
-  }
-
-end
-
-puts HTTParty.post(INSIGHTS_API_URL,
-            :body    => Yajl::Encoder.encode(events),
-            :headers => {'Content-Type' => 'application/json',
-                         'X-Insert-Key' => INSIGHTS_INSERT_KEY})
